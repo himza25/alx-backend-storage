86694f6aa398448bbb9be9e1c604096da4086dae
himza25 <himza.of.theking95@gmail.com>
2024-04-25 16:48:10 -0700

    task 3 & 4

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,8 @@
--- Lists all Glam rock bands by longevity, handling bands still active as of 2022
-SELECT 
-    band_name, 
-    IF(split IS NOT NULL AND split != 0, split - formed, 2022 - formed) AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    main_style = 'Glam rock' 
-ORDER BY 
-    lifespan DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SET @this_year := 2022;  -- Sets the current year for calculating ongoing band lifespans
+
+SELECT band_name,
+       IF(split IS NULL, @this_year - formed, split - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'  -- Allows for partial matches on the style column
+ORDER BY lifespan DESC;

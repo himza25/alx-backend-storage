672968466ba3f821576f6f174bc993c1317cc85e
himza25 <himza.of.theking95@gmail.com>
2024-04-25 15:56:41 -0700

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table `users` with country codes as an enumeration
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);

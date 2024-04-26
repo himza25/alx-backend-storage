0d66092bb13b2b050c3ebe00c6a3b266465db091
himza25 <himza.of.theking95@gmail.com>
2024-04-25 18:28:40 -0700

    task 12

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,31 @@
+-- Drops the existing stored procedure if it exists
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+-- Create the stored procedure ComputeAverageWeightedScoreForUser
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    -- Declare variables to store scores and weights
+    DECLARE total_score FLOAT;
+    DECLARE total_weight INT;
+
+    -- Calculate the sum of weighted scores and total weight
+    SELECT 
+        SUM(c.score * p.weight) INTO total_score,
+        SUM(p.weight) INTO total_weight
+    FROM 
+        corrections c
+    JOIN 
+        projects p ON c.project_id = p.id
+    WHERE 
+        c.user_id = user_id;
+
+    -- Update the average_score in the users table with the weighted average
+    UPDATE users
+    SET average_score = CASE 
+                            WHEN total_weight > 0 THEN total_score / total_weight
+                            ELSE 0
+                        END
+    WHERE id = user_id;
+END //
+DELIMITER ;

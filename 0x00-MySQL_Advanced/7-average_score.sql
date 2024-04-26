516ca2e2167d0c83f5736c82f3aebf8e2e06d23a
himza25 <himza.of.theking95@gmail.com>
2024-04-25 18:04:21 -0700

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,17 +1,16 @@
 -- Drops the existing procedure if it already exists
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
--- Creates a new stored procedure
+-- Resets the delimiter to allow for the entire procedure to be defined as a single command
 DELIMITER //
+
+-- Creates the stored procedure to compute and update the average score
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    -- Declare a variable to store the computed average score
-    DECLARE avg_score FLOAT;
+    -- Declare a session variable to store the computed average score
+    SET @user_avg = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
 
-    -- Compute the average score for the given user_id
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-
-    -- Update the average_score in the users table
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
+    -- Update the average_score field in the users table for the specified user_id
+    UPDATE users SET average_score = @user_avg WHERE id = user_id;
 END //
 DELIMITER ;

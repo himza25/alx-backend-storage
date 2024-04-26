036f128a9becdab0ae799fdcca5646b46a132ea6
himza25 <himza.of.theking95@gmail.com>
2024-04-25 18:47:38 -0700

    task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,16 +1,23 @@
--- Average weighted score 
--- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
+-- Drops the existing procedure if it already exists to avoid conflicts
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+-- Sets the delimiter to allow for complete procedure definition
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
+-- Creates the stored procedure to compute and update the weighted average score
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-        SET @average := (SELECT
-            SUM(corrections.score * projects.weight) / SUM(projects.weight)
-            FROM corrections
-            INNER JOIN projects
-            ON projects.id = corrections.project_id
-            WHERE corrections.user_id = user_id);
-        UPDATE users SET average_score = @average WHERE users.id = user_id; 
+    -- Declare a session variable to store the computed weighted average score
+    SET @average := (SELECT
+                         SUM(c.score * p.weight) / SUM(p.weight)
+                     FROM corrections c
+                     INNER JOIN projects p ON c.project_id = p.id
+                     WHERE c.user_id = user_id
+                     AND p.weight > 0);
+
+    -- Update the users table with the calculated average
+    UPDATE users SET average_score = @average WHERE id = user_id;
 END //
 
+-- Reset the delimiter to the default semicolon
 DELIMITER ;

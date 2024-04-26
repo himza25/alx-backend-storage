2f615794b0628b7ff3004fc77277e83419885692
himza25 <himza.of.theking95@gmail.com>
2024-04-25 17:59:06 -0700

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,14 @@
 -- Drops the existing stored procedure if it exists
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
--- Creates the stored procedure ComputeAverageScoreForUser
+-- Recreate the stored procedure ComputeAverageScoreForUser
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
+    DECLARE avg_score FLOAT DEFAULT 0;
 
-    -- Compute the average score
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+    -- Compute the average score if there are corrections, otherwise leave as default 0
+    SELECT IFNULL(AVG(score), 0) INTO avg_score FROM corrections WHERE user_id = user_id;
 
     -- Update the average score in the users table
     UPDATE users SET average_score = avg_score WHERE id = user_id;

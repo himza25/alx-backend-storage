5cc677afd6383839f6f203192dec68a3d10e6c47
himza25 <himza.of.theking95@gmail.com>
2024-04-25 18:20:09 -0700

    task 8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,7 +1,2 @@
--- SQL script to create an index on the first letter of the name column in the names table
-
--- Drop existing index if it exists to avoid conflicts when recreating or updating the index
-DROP INDEX IF EXISTS idx_name_first ON names;
-
--- Create a new index on the first letter of the name column
+-- SQL script to ensure optimal creation of an index on the first letter of the name column in the names table
 CREATE INDEX idx_name_first ON names (name(1));

8c8167c0329ed76fa1753ae0b19536c119dead33
himza25 <himza.of.theking95@gmail.com>
2024-04-25 18:22:48 -0700

    task 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,7 +1,2 @@
--- SQL script to create a composite index on the first letter of the name and the score
-
--- Drop existing index if it exists to avoid conflicts when recreating or updating the index
-DROP INDEX IF EXISTS idx_name_first_score ON names;
-
--- Create a composite index on the first letter of the name and the score
+-- SQL script to create a composite index on the first letter of the name and the score.
 CREATE INDEX idx_name_first_score ON names (name(1), score);

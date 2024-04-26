40951eefa1c10c68244a80f0f290549ccb412cf1
himza25 <himza.of.theking95@gmail.com>
2024-04-25 18:14:43 -0700

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,10 +1,16 @@
--- Average score 
--- SQL script that creates a stored procedure
+-- Drops the existing procedure if it already exists
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+-- Changes the delimiter to define the stored procedure
 DELIMITER //
 
+-- Creates the stored procedure to compute and update the average score
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    SET @user_avg = (SELECT AVG(score) FROM corrections WHERE corrections.user_id=user_id);
-    UPDATE users SET average_score=@user_avg WHERE id=user_id;
-END//
+    -- Calculate the average score using a session variable
+    SET @user_avg = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
+    
+    -- Update the users table with the calculated average score
+    UPDATE users SET average_score = @user_avg WHERE id = user_id;
+END //
 DELIMITER ;

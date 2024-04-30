3359c8e30c2dd912899ce7993efef12be16d4234
himza25 <himza.of.theking95@gmail.com>
2024-04-30 15:35:08 -0700

    task 9 to 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,26 +5,17 @@ about Nginx logs stored in MongoDB
 """
 
 import pymongo
+if __name__ == "__main__":
+    myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+    mydb = myclient.logs
+    mycol = mydb.nginx
 
-
-def log_stats():
-    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
-    db = client.logs
-    collection = db.nginx
-
-    print(f"{collection.count_documents({})} logs")
+    print("{} logs".format(mycol.count_documents({})))
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+    for m in methods:
+        print("\tmethod {}: {}".format(m, mycol.count_documents({"method": m})))
 
-    status_check = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check} status check")
-
-
-if __name__ == "__main__":
-    log_stats()
+    print("{} status check".format(
+        mycol.count_documents({"method": "GET", "path": "/status"})))

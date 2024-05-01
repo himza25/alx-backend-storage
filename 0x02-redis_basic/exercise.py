dfe9d32e6af4ed743377827809ebef2a8c18b258
himza25 <himza.of.theking95@gmail.com>
2024-05-01 13:51:22 -0700

    tasks exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,3 +23,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis by key. If fn is provided, use it to
+        process the data before returning; otherwise, return as bytes.
+        """
+        data = self._redis.get(key)
+        if data is not None and fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve a string from Redis."""
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve an integer from Redis."""
+        return self.get(key, int)

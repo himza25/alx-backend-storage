14c95f60c6137e8429b02a87806f8c3f750dd868
himza25 <himza.of.theking95@gmail.com>
2024-05-01 15:37:57 -0700

    task web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,43 @@
 #!/usr/bin/env python3
-"""Module for caching and tracking web page accesses using Redis."""
+"""
+Module for caching and tracking web page accesses using Redis.
+Implements a decorator to handle caching and count tracking.
+"""
 
 import requests
 from redis import Redis
 from functools import wraps
+
 
 # Setup the Redis connection
 redis = Redis(host='localhost', port=6379, db=0)
 
 
 def cache(func):
-    """Decorator to cache pages and track page access counts."""
+    """
+    Decorate a function to cache its web responses and track access counts.
+    Each accessed URL has its content cached and access count incremented.
+    """
     @wraps(func)
     def wrapper(url: str) -> str:
-        """Retrieve web page from cache or fetch and cache it."""
+        """
+        Wrapper function to manage caching. If the requested URL is in the
+        cache, return the cached content, otherwise fetch, cache, and return.
+        """
         count_key = f"count:{url}"
         cache_key = f"cache:{url}"
 
-        # Increment the access count
+        # Increment the access count for the URL
         redis.incr(count_key)
 
-        # Attempt to retrieve the cached content
+        # Check if the URL is already cached
         cached_content = redis.get(cache_key)
         if cached_content:
             return cached_content.decode('utf-8')
 
-        # If cache miss, fetch the content and cache it
+        # Fetch, cache, and return the content if not cached
         page_content = func(url)
-        redis.setex(cache_key, 10, page_content)  # Expire after 10 seconds
+        redis.setex(cache_key, 10, page_content)
         return page_content
 
     return wrapper
@@ -35,13 +45,16 @@ def cache(func):
 
 @cache
 def get_page(url: str) -> str:
-    """Fetch HTML content of a URL, caching the results."""
+    """
+    Fetch the HTML content of a URL. This function is decorated to use Redis
+    for caching the content and tracking how many times a URL has been accessed
+    """
     response = requests.get(url)
     return response.text
 
 
 if __name__ == "__main__":
-    # Example URL that simulates a slow response
+    # Test the function with a URL that simulates a slow response
     slow_url = ("http://slowwly.robertomurray.co.uk/delay/5000/url/"
                 "http://www.google.com")
     print(get_page(slow_url))

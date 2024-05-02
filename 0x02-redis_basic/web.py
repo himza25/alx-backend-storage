40c735b848d0cb7c6859e2fe1039da422bc086a8
himza25 <himza.of.theking95@gmail.com>
2024-05-01 17:03:13 -0700

    task web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,49 @@
 #!/usr/bin/env python3
 """
-Caching request module
+Module to implement a simple web page fetcher with caching and access counting
+using Redis.
 """
-import redis
 import requests
+import redis
 from functools import wraps
 from typing import Callable
 
+# Create a Redis client instance
+_redis = redis.Redis()
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
+
+def count_requests(method: Callable) -> Callable:
+    """Decorator to count the number of times a method is called."""
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        url = args[0]
+        count_key = f"count:{url}"
+        _redis.incr(count_key)
+        return method(*args, **kwargs)
     return wrapper
 
 
-@track_get_page
+def cache_page(method: Callable) -> Callable:
+    """Decorator to cache the page content."""
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        cache_key = f"cache:{url}"
+        cached_content = _redis.get(cache_key)
+        if cached_content:
+            return cached_content.decode('utf-8')
+        else:
+            content = method(url)
+            _redis.setex(cache_key, 10, content)
+            return content
+    return wrapper
+
+
+@count_requests
+@cache_page
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
+    """
+    Obtain the HTML content of a URL and return it. Keep track of the number of
+    times the URL was accessed and cache the content with an expiration time.
     """
     response = requests.get(url)
     return response.text
